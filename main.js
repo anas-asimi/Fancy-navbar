e92fa42830cd1897b3166c8c96e5060d8bbec5f7
anas0bito <96184837+anas0bito@users.noreply.github.com>
2022-05-04 17:28:41 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
-
 function Nav_fun() {
+    var IsNavOpened = document.getElementById("nav-items").style.transform === "translateX(0%)"
     // 
-    // 
-    if (document.getElementById("nav-items").style.transform === "translateX(0%)") {
+    if (IsNavOpened) {
 
         document.getElementById("nav-items").style.transform = "translateX(100%)" ;
         document.getElementById("menu-btn").className = "fa-solid fa-angles-left fa-2x";
-        document.querySelector("body").style.overflow = "hidden" ;
-    }
-    // 
-    // 
+    } 
     else{
-
         document.getElementById("nav-items").style.transform = "translateX(0%)" ;
         document.getElementById("menu-btn").className = "fa-solid fa-angles-right fa-2x";
-        document.querySelector("body").style.overflow = "hidden" ;
     }
-
 }
-// 
-// 
-// var IsNavOpened = Boolean(false);
-// function navbar() {
-//     if (IsNavOpened) {
-//         alert(true);
-//     }
-//     else{
-//         alert(false);
-//     }
-//     IsNavOpened = !IsNavOpened
-// }
-
